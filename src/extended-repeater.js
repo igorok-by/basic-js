cf927613ac473c907daa8acdd2a5d173d34b86f7
Igor <igorko.by@gmail.com>
2020-03-10 23:24:09 +0300

    feat: implement 'extended-repeater' task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,44 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  let resultArr = [];
+  let additionArr = [];
+
+  if (options.separator === undefined) options.separator = '+';
+  if (str === null) str = 'null';
+  if (options.addition === null) options.addition = 'null';
+
+
+  if (options.addition === undefined) {
+    let count = 0;
+
+    while (count < options.repeatTimes) {
+      resultArr.push(`${str}`);
+      count++;
+    };
+    return resultArr.join(options.separator);
+
+
+  } else {
+    let count = 0;
+
+    if (options.repeatTimes === undefined) options.repeatTimes = 1;
+    if (options.additionRepeatTimes === undefined) options.additionRepeatTimes = 1;
+    if (options.additionSeparator === undefined) options.additionSeparator = '|';
+
+    while (count < options.additionRepeatTimes) {
+      additionArr.push(options.addition);
+      count++;
+    };
+
+    additionArr.join(options.additionSeparator);
+    count = 0;
+
+    while (count < options.repeatTimes) {
+      resultArr.push(additionArr.join(options.additionSeparator));
+      count++;
+    };
+  }
+
+  return resultArr.map(arrElem => `${str}` + arrElem).join(options.separator);
 };
   
